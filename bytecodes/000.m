d47d6e6ac9a1258ec2a7870eed82713fc52598db
Tau <taumasaire@gmail.com>
2023-11-15 14:35:53 +0200

    updating push function

---
+++ bytecodes/000.m
@@ -0,0 +1,16 @@
+push 8
+push 13
+pint      
+push 7 Push 1 onto the stack
+push 2
+pint    
+  push 3
+                   pall    
+push 99
+
+   
+                           
+push 4
+
+    push 5    
+      push    6        
--- push.c
+++ push.c
@@ -1,74 +1,26 @@
-#define _POSIX_C_SOURCE 200809L
 #include "monty.h"
 
-int main(int argc, char **argv);
 /**
- * main - entry point
- * @argc: number of command line arguments
- * @argv: an array of strings
- * Return: Always 0 Success
+ * push - pushs a node onto a stack
+ * @stack: pointer to a stack
+ * @line_number: an integer
+ * @val: an integer
+ * Return: Nothing
  */
-int main(int argc, char **argv)
+void push(stack_t **stack, unsigned int line_number, int val)
 {
-	FILE *stream;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t cread;
-	int line_number = 1;
-	char *opcode, *token;
-	stack_t *head = NULL;
-	instruction_t instr;
-	stack_t **stack = &head;
+	stack_t *new_node = (stack_t *)malloc(sizeof(stack_t));
+	(void)line_number;
 
-	if (argc != 2)
+	if (new_node == NULL)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	stream = fopen(argv[1], "r");
-	if (stream == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while ((cread = getline(&line, &len, stream)) != -1)
-	{
-		token = strtok(line, " ");
-		opcode = token;
-		token = strtok(NULL, " ");
 
-		instr.opcode = opcode;
-		if (strcmp(opcode, "push") == 0)
-		{
-			if (token == NULL || !isdigit(*token))
-			{
-				fprintf(stderr, "L%d: usage: push integer\n", line_number);
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				instr.val = atoi(token);
-				instr.f = push;
-			}
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			instr.f = pall;
-		}
-		else if (strcmp(opcode, "pint") == 0)
-		{
-			instr.f = pint;
-		}
-		else
-		{
-			fprintf(stderr, "L%d: Unknown instruction %s\n", line_number, opcode);
-			exit(EXIT_FAILURE);
-		}
-		instr.f(stack, line_number, instr.val);
-		line_number++;
-	}
-	free(line);
-	fclose(stream);
-	exit(EXIT_SUCCESS);
+	new_node->n = val;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	*stack = new_node;
 }
-
